b9317ad91015259348eff1b3319e0ee44e1efb24
kienh <kienhue98@gmail.com>
2024-01-04 12:38:51 +0700

    fix: route edit user

--- routes/permissions/super_admin.php
+++ routes/permissions/super_admin.php
@@ -1,14 +1,17 @@
 <?php
 
 use App\Http\Controllers\AddressController;
+use App\Http\Livewire\advance_config\Address;
+use App\Http\Livewire\advance_config\UserManagement;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'address'], function () {
-    Route::get('index', \App\Http\Livewire\advance_config\Address::class)->name('address.index');
-    Route::post('store', [AddressController::class,'store'])->name('address.store');
+    Route::get('index', Address::class)->name('address.index');
+    Route::post('store', [AddressController::class, 'store'])->name('address.store');
 });
 
 Route::group(['prefix' => 'user'], function () {
-    Route::get('index', \App\Http\Livewire\advance_config\UserManagement::class)->name('user.index');
-    Route::post('store', [AddressController::class,'store'])->name('user.store');
+    Route::get('index', UserManagement::class)->name('user.index');
+    Route::post('store', [AddressController::class, 'store'])->name('user.store');
+    Route::post('edit', [AddressController::class, 'edit'])->name('user.edit');
 });
