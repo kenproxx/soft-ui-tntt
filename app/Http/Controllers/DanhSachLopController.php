2e7523d5fed42aeadfdf64fb1aebb92e47695360
w <kienhue98@gmail.com>
2024-01-07 15:11:27 +0700

    feat: model, controller, migration Danh sach lop

---
+++ app/Http/Controllers/DanhSachLopController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\DanhSachLop;
+use Illuminate\Http\Request;
+
+class DanhSachLopController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(DanhSachLop $danhSachLop)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(DanhSachLop $danhSachLop)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, DanhSachLop $danhSachLop)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(DanhSachLop $danhSachLop)
+    {
+        //
+    }
+}
---
+++ app/Models/DanhSachLop.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\CreatedUpdatedBy;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DanhSachLop extends Model
+{
+    use HasFactory;
+    use CreatedUpdatedBy;
+
+}
