264ea984402678418cf11a651f1b167f49504977
w <kienhue98@gmail.com>
2024-01-07 15:08:33 +0700

    feat: man hinh danh sach lop

---
+++ app/Http/Livewire/DoanSinh/DanhSachLopIndex.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\DoanSinh;
+
+use Livewire\Component;
+
+class DanhSachLopIndex extends Component
+{
+    public function render()
+    {
+        return view('livewire.doan-sinh.danh-sach-lop-index');
+    }
+}
--- routes/permissions/admin.php
+++ routes/permissions/admin.php
@@ -6,14 +6,15 @@ use App\Http\Livewire\advance_config\UserEdit;
 use App\Http\Livewire\advance_config\UserManagement;
 use Illuminate\Support\Facades\Route;
 
-Route::group(['prefix' => 'user'], function () {
+Route::group(['prefix' => 'tai-khoan'], function () {
     Route::get('index', UserManagement::class)->name('user.index');
     Route::post('store', [UserController::class, 'store'])->name('user.store');
-    Route::get('edit/{id}', UserEdit::class)->name('user.edit');
+    Route::get('sua/{id}', UserEdit::class)->name('user.edit');
     Route::post('update/{id}', [UserController::class, 'update'])->name('user.update');
 });
 
 Route::group(['prefix' => 'doan-sinh'], function () {
     Route::get('thieu-nhi', \App\Http\Livewire\DoanSinh\ThieuNhiIndex::class)->name('doan-sinh.thieu-nhi');
     Route::get('huynh-truong', \App\Http\Livewire\DoanSinh\HuynhTruongIndex::class)->name('doan-sinh.huynh-truong');
+    Route::get('danh-sach-lop', \App\Http\Livewire\DoanSinh\DanhSachLopIndex::class)->name('doan-sinh.danh-sach-lop');
 });
