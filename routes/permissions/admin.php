9b2d9eb96a077db23228a43388dc50e55a20f1c9
w <kienhue98@gmail.com>
2024-01-13 22:57:19 +0700

    fix: import class trong route

--- routes/permissions/admin.php
+++ routes/permissions/admin.php
@@ -1,9 +1,13 @@
 <?php
 
 
+use App\Http\Controllers\DanhSachLopController;
 use App\Http\Controllers\UserController;
 use App\Http\Livewire\advance_config\UserEdit;
 use App\Http\Livewire\advance_config\UserManagement;
+use App\Http\Livewire\DoanSinh\DanhSachLopIndex;
+use App\Http\Livewire\DoanSinh\HuynhTruongIndex;
+use App\Http\Livewire\DoanSinh\ThieuNhiIndex;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'tai-khoan'], function () {
@@ -14,9 +18,9 @@ Route::group(['prefix' => 'tai-khoan'], function () {
 });
 
 Route::group(['prefix' => 'doan-sinh'], function () {
-    Route::get('thieu-nhi', \App\Http\Livewire\DoanSinh\ThieuNhiIndex::class)->name('doan-sinh.thieu-nhi');
-    Route::get('huynh-truong', \App\Http\Livewire\DoanSinh\HuynhTruongIndex::class)->name('doan-sinh.huynh-truong');
-    Route::get('danh-sach-lop', \App\Http\Livewire\DoanSinh\DanhSachLopIndex::class)->name('doan-sinh.danh-sach-lop');
-    Route::post('danh-sach-lop/store', [\App\Http\Controllers\DanhSachLopController::class, 'store'])->name('doan-sinh.danh-sach-lop.store');
-    Route::post('danh-sach-lop/add-member', [\App\Http\Controllers\DanhSachLopController::class, 'addMemberToClass'])->name('doan-sinh.danh-sach-lop.add-member');
+    Route::get('thieu-nhi', ThieuNhiIndex::class)->name('doan-sinh.thieu-nhi');
+    Route::get('huynh-truong', HuynhTruongIndex::class)->name('doan-sinh.huynh-truong');
+    Route::get('danh-sach-lop', DanhSachLopIndex::class)->name('doan-sinh.danh-sach-lop');
+    Route::post('danh-sach-lop/store', [DanhSachLopController::class, 'store'])->name('doan-sinh.danh-sach-lop.store');
+    Route::post('danh-sach-lop/add-member', [DanhSachLopController::class, 'addMemberToClass'])->name('doan-sinh.danh-sach-lop.add-member');
 });
--- routes/permissions/normal.php
+++ routes/permissions/normal.php
@@ -1,11 +1,12 @@
 <?php
 
 
+use App\Http\Controllers\AddressController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'thong-tin-dia-chi'], function () {
-    Route::get('giao-phan', [\App\Http\Controllers\AddressController::class, 'getGiaoPhan'])->name('dia-chi.giao-phan');
-    Route::get('giao-hat/{id}', [\App\Http\Controllers\AddressController::class, 'getGiaoHatByGiaoPhan'])->name('dia-chi.giao-hat');
-    Route::get('giao-xu/{id}', [\App\Http\Controllers\AddressController::class, 'getGiaoXuByGiaoHat'])->name('dia-chi.giao-xu');
-    Route::get('giao-ho/{id}', [\App\Http\Controllers\AddressController::class, 'getGiaoHoByGiaoXu'])->name('dia-chi.giao-ho');
+    Route::get('giao-phan', [AddressController::class, 'getGiaoPhan'])->name('dia-chi.giao-phan');
+    Route::get('giao-hat/{id}', [AddressController::class, 'getGiaoHatByGiaoPhan'])->name('dia-chi.giao-hat');
+    Route::get('giao-xu/{id}', [AddressController::class, 'getGiaoXuByGiaoHat'])->name('dia-chi.giao-xu');
+    Route::get('giao-ho/{id}', [AddressController::class, 'getGiaoHoByGiaoXu'])->name('dia-chi.giao-ho');
 });
